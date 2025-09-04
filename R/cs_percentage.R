fd8c954c02f08daa5edb67376da73fb8a3cedf93
Benedikt Claus <benediktclaus@msn.com>
2025-09-04 09:19:57 +0200

    Update print and summary for percentage

--- R/cs_percentage.R
+++ R/cs_percentage.R
@@ -246,44 +246,38 @@ cs_percentage <- function(
 #'
 #' cs_results
 print.cs_percentage <- function(x, ...) {
-  summary_table <- x[["summary_table"]]
-  pct_improvement <- x[["pct_improvement"]] * 100
-  pct_deterioration <- x[["pct_deterioration"]] * 100
-  direction <- x[["direction"]]
+  summary_table <- .format_summary_table(x[["summary_table"]])
+  pct_improvement <- insight::format_percent(x[["pct_improvement"]])
+  pct_deterioration <- insight::format_percent(x[["pct_deterioration"]])
 
-  if (direction == -1) {
-    dir_improvement <- "decrease"
+  if (x[["direction"]] == -1) {
+    direction <- "Lower"
   } else {
-    dir_improvement <- "increase"
-  }
-  if (direction == -1) {
-    dir_deterioration <- "increase"
-  } else {
-    dir_deterioration <- "decrease"
+    direction <- "Higher"
   }
 
-  outcome <- x[["outcome"]]
+  model_info <- .format_model_info_string(
+    list(
+      Approach = "Percentage-based",
+      "Percentage Improvement" = pct_improvement,
+      "Percentage Deterioration" = pct_deterioration,
+      "Better is" = direction
+    )
+  )
 
-  if (pct_improvement == pct_deterioration) {
-    pct_string <- "{.strong {pct_improvement}%} {dir_improvement} in instrument scores indicating a clinical significant improvement."
-  } else {
-    pct_string <- "{.strong {pct_improvement}%} {dir_improvement} in instrument scores indicating a clinical significant improvement and a {.strong {pct_deterioration}%} {dir_deterioration} in instrument scores indicating a clinical significant deterioration."
-  }
-
-  summary_table_formatted <- summary_table |>
-    dplyr::mutate(
-      dplyr::across(dplyr::contains("percent"), \(a) insight::format_percent(a))
-    ) |>
-    dplyr::rename_with(snakecase::to_title_case)
+  .print_strings(
+    model_info,
+    summary_table
+  )
 
   # Print output
-  output_fun <- function() {
-    cli::cli_h2("Clinical Significance Results")
-    cli::cli_text(c("Percentage-change approach with a ", pct_string))
-    cli::cat_line()
-    cli::cli_verbatim(insight::export_table(summary_table_formatted))
-  }
-  output_fun()
+  # output_fun <- function() {
+  #   cli::cli_h2("Clinical Significance Results")
+  #   cli::cli_text(c("Percentage-change approach with a ", pct_string))
+  #   cli::cat_line()
+  #   cli::cli_verbatim(insight::export_table(summary_table_formatted))
+  # }
+  # output_fun()
 }
 
 
@@ -309,53 +303,38 @@ print.cs_percentage <- function(x, ...) {
 #' summary(cs_results)
 summary.cs_percentage <- function(object, ...) {
   # Get necessary information from object
-  summary_table <- object[["summary_table"]] |>
-    dplyr::mutate(
-      dplyr::across(dplyr::contains("percent"), \(a) insight::format_percent(a))
-    ) |>
-    dplyr::rename_with(snakecase::to_title_case)
+  summary_table <- .format_summary_table(object[["summary_table"]])
+  pct_improvement <- insight::format_percent(object[["pct_improvement"]])
+  pct_deterioration <- insight::format_percent(object[["pct_deterioration"]])
 
-  pct_improvement <- object[["pct_improvement"]] * 100
-  pct_deterioration <- object[["pct_deterioration"]] * 100
+  if (object[["direction"]] == -1) {
+    direction <- "Lower"
+  } else {
+    direction <- "Higher"
+  }
+
   n_original <- cs_get_n(object, "original")[[1]]
   n_used <- cs_get_n(object, "used")[[1]]
   pct <- round(n_used / n_original, digits = 3) * 100
-  direction <- object[["direction"]]
-
-  if (direction == -1) {
-    dir_improvement <- "decrease"
-  } else {
-    dir_improvement <- "increase"
-  }
-  if (direction == -1) {
-    dir_deterioration <- "increase"
-  } else {
-    dir_deterioration <- "decrease"
-  }
-
   outcome <- object[["outcome"]]
 
-  if (pct_improvement == pct_deterioration) {
-    pct_string <- "{.strong {pct_improvement}%} {dir_improvement} in instrument scores indicating a clinical significant improvement."
-  } else {
-    pct_string <- "{.strong {pct_improvement}%} {dir_improvement} in instrument scores indicating a clinical significant improvement and a {.strong {pct_deterioration}%} {dir_deterioration} in instrument scores indicating a clinical significant deterioration."
-  }
+  model_info <- .format_model_info_string(
+    list(
+      Approach = "Percentage-based",
+      "Percentage Improvement" = pct_improvement,
+      "Percentage Deterioration" = pct_deterioration,
+      "Better is" = direction,
+      "N (original)" = n_original,
+      "N (used)" = n_used,
+      "Percent used" = insight::format_percent(n_used / n_original),
+      Outcome = outcome
+    )
+  )
 
   # Print output
-  output_fun <- function() {
-    cli::cli_h2("Clinical Significance Results")
-    cli::cli_text(c(
-      "Percentage-change analysis of clinical significance with a ",
-      pct_string
-    ))
-    cli::cat_line()
-    cli::cli_text(
-      "There were {.strong {n_original}} participants in the whole dataset of which {.strong {n_used}} {.strong ({pct}%)} could be included in the analysis."
-    )
-    cli::cat_line()
-    cli::cli_h3("Individual Level Results")
-    cli::cat_line()
-    cli::cli_verbatim(insight::export_table(summary_table))
-  }
-  output_fun()
+
+  .print_strings(
+    model_info,
+    summary_table
+  )
 }
